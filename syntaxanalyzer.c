e22f6f232a35fabe9aa23b992f49533d56b7b727
André Vincent <andrevncnt7@gmail.com>
2019-04-09 11:39:25 -0400

    begins global declarations

--- syntaxanalyzer.c
+++ syntaxanalyzer.c
@@ -1,11 +1,18 @@
 /* a lexical analyzer system for simple arithmetic expressions */
 #include <stdio.h>
 #include <ctype.h>
+/* Global declarations */
+/* Variables */
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int token;
 int main () {
 	/* open input data file */
 	FILE * MyFile;
 	MyFile = fopen("arith.txt", "r");
-
+	// singleLine holds the line retrieved from the data file
 	char singleLine[150];
 	while (!feof(MyFile)) {
 		fgets(singleLine, 150, MyFile);
